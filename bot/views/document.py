0a711a893d596f3b91f61a26b16a283cf6ac813f
drforse <george.lifeslice@gmail.com>
2020-07-20 14:04:23 +0100

    handle text and files in pm without command

---
+++ bot/views/document.py
@@ -0,0 +1,19 @@
+import os
+from aiogram_oop_framework.views import DocumentView
+
+from pastebin.hatebin import Hatebin
+
+
+class MyDocView(DocumentView):
+    index = 3
+    custom_filters = [lambda m: m.chat.type == 'private']
+
+    @classmethod
+    async def execute(cls, m, state=None, **kwargs):
+        pastebin = Hatebin(m.bot.session)
+        dest = await m.document.download()
+        dest.close()
+        with open(dest.name) as file:
+            url = await pastebin.create_paste_from_file(file)
+        os.remove(dest.name)
+        await m.reply(url)
